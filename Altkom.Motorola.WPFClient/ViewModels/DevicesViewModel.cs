e99d3dcf3773fbfe328cb3beb103c2b8d43bdc2e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-12-09 13:39:43 +0100

    init

---
+++ Altkom.Motorola.WPFClient/ViewModels/DevicesViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Altkom.Motorola.IServices;
+using Altkom.Motorola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.Motorola.WPFClient.ViewModels
+{
+    class DevicesViewModel
+    {
+        private ICollection<Device> devices;
+
+
+
+        public DevicesViewModel(IDevicesService devicesService)
+        {
+            devices = devicesService.Get();
+        }
+    }
+}
